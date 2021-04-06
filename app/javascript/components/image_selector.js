1042d1d25f64a613c6c2b8a8ea7318dd56764655
Alex Marriott <marriottaog@gmail.com>
2021-04-06 12:00:22 +0100

    app now allows you to flip cards and recognises two matching cards, but no logic for card unflipping yet

--- app/javascript/components/image_selector.js
+++ app/javascript/components/image_selector.js
@@ -7,13 +7,21 @@ const imageSelector = () => {
   document.querySelectorAll('.hidden').forEach(image => {
     image.addEventListener('click', event => {
       image.classList.toggle('hidden');
-      let activeImage = document.getElementsByClassName('active')[0]
+      const activeImage = document.getElementsByClassName('active')[0]
 
       if (activeImage && image.src === activeImage.src) {
-        image.classList.add('complete')
-        activeImage.classList.add('complete')
-        image.parentElement.style.backgroundImage = 'url("https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Dark_green.svg/1200px-Dark_green.svg.png")'
-        activeImage.parentElement.style.backgroundImage = 'url("https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Dark_green.svg/1200px-Dark_green.svg.png")'
+        image.classList.add('complete');
+        activeImage.classList.add('complete');
+
+        image.parentElement.style.backgroundImage = 'url("https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Dark_green.svg/1200px-Dark_green.svg.png")';
+        activeImage.parentElement.style.backgroundImage = 'url("https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Dark_green.svg/1200px-Dark_green.svg.png")';
+
+        image.parentElement.style.borderRadius = '24px'
+        activeImage.parentElement.style.borderRadius = '24px'
+
+        activeImage.classList.remove('active');
+      } else if (1 === 1) {
+
       } else {
         image.classList.add('active');
       }
@@ -27,3 +35,4 @@ export default imageSelector;
 // as we build images they get assigned class like image1 image 2
 // add logic in case they press same image twice
 // maybe change active image to const
+// need another function to always check if its been completed
