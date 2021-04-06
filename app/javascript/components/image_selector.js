e8e343ff1e209454363b64cbfff43ddc8dad7c2f
Alex Marriott <marriottaog@gmail.com>
2021-04-06 11:49:39 +0100

    images now have card background as default and change to green background if you get two successfully

--- app/javascript/components/image_selector.js
+++ app/javascript/components/image_selector.js
@@ -7,11 +7,13 @@ const imageSelector = () => {
   document.querySelectorAll('.hidden').forEach(image => {
     image.addEventListener('click', event => {
       image.classList.toggle('hidden');
-      if (document.getElementsByClassName('active')[0]) {
-        console.log('first check')
-        if (image.src === document.getElementsByClassName('active')[0].src) {
-          console.log('second check')
-        }
+      let activeImage = document.getElementsByClassName('active')[0]
+
+      if (activeImage && image.src === activeImage.src) {
+        image.classList.add('complete')
+        activeImage.classList.add('complete')
+        image.parentElement.style.backgroundImage = 'url("https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Dark_green.svg/1200px-Dark_green.svg.png")'
+        activeImage.parentElement.style.backgroundImage = 'url("https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Dark_green.svg/1200px-Dark_green.svg.png")'
       } else {
         image.classList.add('active');
       }
@@ -24,3 +26,4 @@ export default imageSelector;
 
 // as we build images they get assigned class like image1 image 2
 // add logic in case they press same image twice
+// maybe change active image to const
