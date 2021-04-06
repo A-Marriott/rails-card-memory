3fb1572ab3f6f64adfb78f5b539c0ff82b1366cb
Alex Marriott <marriottaog@gmail.com>
2021-04-06 17:09:07 +0100

    app now checks for completion and creates pop up when you win

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,11 @@
 class PagesController < ApplicationController
   def home
-    @images = ["https://bit.ly/2Q4JPVG",
-               "https://bit.ly/3dGFLDd",
-               "https://bit.ly/31SFkjK",
-               "https://bit.ly/3rW160P",
-               "https://bit.ly/3ujxdsS",
-               "https://bit.ly/3cTULif"]
+    @images = ["https://bit.ly/2Q4JPVG"]
+               # "https://bit.ly/3dGFLDd",
+               # "https://bit.ly/31SFkjK",
+               # "https://bit.ly/3rW160P",
+               # "https://bit.ly/3ujxdsS",
+               # "https://bit.ly/3cTULif"]
     @randomised_images = @images.map { |image| [image, image] }.flatten.shuffle
   end
 end
--- app/javascript/components/image_selector.js
+++ app/javascript/components/image_selector.js
@@ -1,25 +1,27 @@
-// document.querySelectorAll('.some-class').forEach(item => {
-//   item.addEventListener('click', event => {
-//     //handle click
-//   })
-// })
+const completionCheck = () => {
+  if (document.querySelectorAll('.complete').length === document.querySelectorAll('.card-holder').length) {
+    setTimeout(() => alert("You won!"), 0.01);
+    location.reload();
+  }
+};
+
 const imageSelector = () => {
   document.querySelectorAll('.hidden').forEach(image => {
     image.addEventListener('click', event => {
       image.classList.remove('hidden');
       const activeImage = document.getElementsByClassName('active')[0]
         if (image.classList.contains('active') || image.classList.contains('complete')) {
-
+          // do nothing
         } else if (activeImage && image.src === activeImage.src) {
           activeImage.classList.remove('active');
-
           window.setTimeout(() => {
             image.classList.add('complete');
             activeImage.classList.add('complete');
 
             image.parentElement.style.backgroundImage = 'url("https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Dark_green.svg/1200px-Dark_green.svg.png")';
             activeImage.parentElement.style.backgroundImage = 'url("https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Dark_green.svg/1200px-Dark_green.svg.png")';
-          }, 500);
+            completionCheck();
+          }, 300);
         } else if (activeImage) {
           activeImage.classList.remove('active');
           window.setTimeout(() => {
@@ -34,6 +36,3 @@ const imageSelector = () => {
 };
 
 export default imageSelector;
-
-// need another function to always check if its been completed
-// add logic for pressing image thats already completed
