52d2326c6f38483f82f70cbc8c70786182d09809
Alex Marriott <marriottaog@gmail.com>
2021-04-06 16:00:09 +0100

    changed two images that had white backgrounds

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,8 @@
 class PagesController < ApplicationController
   def home
-    @images = ["https://bit.ly/39MzS6w",
+    @images = ["https://bit.ly/2Q4JPVG",
                "https://bit.ly/3dGFLDd",
-               "https://bit.ly/3rXxMqy",
+               "https://bit.ly/31SFkjK",
                "https://bit.ly/3rW160P",
                "https://bit.ly/3ujxdsS",
                "https://bit.ly/3cTULif"]
