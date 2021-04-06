b431b896449794405ed0d6f456128755a4b1a60d
Alex Marriott <marriottaog@gmail.com>
2021-04-06 15:26:08 +0100

    cards now display nicely on page, increased display time on clicked cards

--- app/javascript/components/image_selector.js
+++ app/javascript/components/image_selector.js
@@ -16,16 +16,13 @@ const imageSelector = () => {
         image.parentElement.style.backgroundImage = 'url("https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Dark_green.svg/1200px-Dark_green.svg.png")';
         activeImage.parentElement.style.backgroundImage = 'url("https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Dark_green.svg/1200px-Dark_green.svg.png")';
 
-        image.parentElement.style.borderRadius = '24px'
-        activeImage.parentElement.style.borderRadius = '24px'
-
         activeImage.classList.remove('active');
       } else if (activeImage) {
         activeImage.classList.remove('active');
         window.setTimeout(() => {
           image.classList.add('hidden');
           activeImage.classList.add('hidden');
-        }, 500);
+        }, 800);
       } else {
         image.classList.add('active');
       }
