3f9f9f324f6834c4a306fa8a529b6f854f7a12c5
Alex Marriott <marriottaog@gmail.com>
2021-04-06 16:46:14 +0100

    app now handles pressing of same image two times in a row

--- app/javascript/components/image_selector.js
+++ app/javascript/components/image_selector.js
@@ -8,32 +8,32 @@ const imageSelector = () => {
     image.addEventListener('click', event => {
       image.classList.remove('hidden');
       const activeImage = document.getElementsByClassName('active')[0]
+        if (image.classList.contains('active')) {
 
-      if (activeImage && image.src === activeImage.src) {
-        activeImage.classList.remove('active');
+        } else if (activeImage && image.src === activeImage.src) {
+          activeImage.classList.remove('active');
 
-        window.setTimeout(() => {
-          image.classList.add('complete');
-          activeImage.classList.add('complete');
+          window.setTimeout(() => {
+            image.classList.add('complete');
+            activeImage.classList.add('complete');
 
-          image.parentElement.style.backgroundImage = 'url("https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Dark_green.svg/1200px-Dark_green.svg.png")';
-          activeImage.parentElement.style.backgroundImage = 'url("https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Dark_green.svg/1200px-Dark_green.svg.png")';
-        }, 500);
-      } else if (activeImage) {
-        activeImage.classList.remove('active');
-        window.setTimeout(() => {
-          image.classList.add('hidden');
-          activeImage.classList.add('hidden');
-        }, 800);
-      } else {
-        image.classList.add('active');
-      }
+            image.parentElement.style.backgroundImage = 'url("https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Dark_green.svg/1200px-Dark_green.svg.png")';
+            activeImage.parentElement.style.backgroundImage = 'url("https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Dark_green.svg/1200px-Dark_green.svg.png")';
+          }, 500);
+        } else if (activeImage) {
+          activeImage.classList.remove('active');
+          window.setTimeout(() => {
+            image.classList.add('hidden');
+            activeImage.classList.add('hidden');
+          }, 800);
+        } else {
+          image.classList.add('active');
+        }
     })
   })
 };
 
 export default imageSelector;
 
-// add logic in case they press same image twice
 // need another function to always check if its been completed
 // add logic for pressing image thats already completed
